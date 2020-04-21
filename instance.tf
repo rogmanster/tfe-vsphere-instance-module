1df25ec90ec85a4cc07233fb71aed85ce93d575d
Roger Chao <rogman@gmail.com>
2020-04-21 15:09:53 -0700

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,14 @@
+module "example-server-linuxvm" {
+  source        = "Terraform-VMWare-Modules/vm/vsphere"
+  version       = "1.0.2"
+  vmtemp        = "TemplateName"
+  instances     = 1
+  vmname        = "example-server-windows"
+  vmrp          = "esxi/Resources"
+  network_cards = ["Name of the Port Group in vSphere"]
+  ipv4 = {
+    "Name of the Port Group in vSphere" = ["10.0.0.1"] # To use DHCP create Empty list for each instance
+  }
+  dc        = "Datacenter"
+  datastore = "Data Store name(use ds_cluster for datastore cluster)"
+}
