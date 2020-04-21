518f28edb98a5be88a7c6423e53381af7879a4a5
Roger Chao <rogman@gmail.com>
2020-04-21 15:19:51 -0700

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,14 +1,20 @@
-module "example-server-linuxvm" {
-  source        = "Terraform-VMWare-Modules/vm/vsphere"
-  version       = "1.0.2"
-  vmtemp        = "TemplateName"
-  instances     = 1
-  vmname        = "example-server-windows"
-  vmrp          = "esxi/Resources"
-  network_cards = ["Name of the Port Group in vSphere"]
+module "vm-PMR" {
+  source  = "ptfe.this-demo.rocks/rchao-demo/vm-PMR/vsphere"
+  version = "1.0.2"
+  
+  vmtemp            = "win2012-new"
+  is_windows_image  = "true"
+  instances         = 1
+  cpu_number        = 2
+  ram_size          = 1024
+  vmname            = "example-server-windows"
+  vmrp              = "esxi/Resources"
+  thin_provisioned  = ["true"]
+  network_cards     = ["Name of the Port Group in vSphere"]
   ipv4 = {
-    "Name of the Port Group in vSphere" = ["10.0.0.1"] # To use DHCP create Empty list for each instance
+    "Name of the Port Group in vSphere" = [] # To use DHCP create Empty list for each instance
   }
   dc        = "Datacenter"
   datastore = "Data Store name(use ds_cluster for datastore cluster)"
+
 }
