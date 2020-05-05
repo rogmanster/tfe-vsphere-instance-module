07d05f0e45516299b92c8311b51ceb2380cc8462
rogman <rogman@0.hsd1.ca.comcast.net>
2020-05-05 16:14:25 -0700

    stage

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-variable "name" {
-  default = "rchao"
-}
-
 provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-module "vm-PMR-${var.name}" {
+module "vm-PMR" {
   source  = "ptfe.this-demo.rocks/rchao-demo/vm-PMR/vsphere"
   version = "1.0.2"
 
